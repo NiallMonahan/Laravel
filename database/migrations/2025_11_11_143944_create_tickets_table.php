829c46d67d061fa6354b74fb2e424e653c58049a
Niall Monahan <N00242247@iadt.ie>
2025-11-11 14:41:56 +0000

    Added content to ticket migration

--- database/migrations/2025_11_11_143944_create_tickets_table.php
+++ database/migrations/2025_11_11_143944_create_tickets_table.php
@@ -4,8 +4,7 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-return new class extends Migration
-{
+return new class extends Migration {
     /**
      * Run the migrations.
      */
@@ -13,7 +12,12 @@ return new class extends Migration
     {
         Schema::create('tickets', function (Blueprint $table) {
             $table->id();
+            $table->foreignId('event_id')->constrained()->onDelete('cascade');
+            $table->string('holder_name');
+            $table->string('seat_number')->nullable();
+            $table->decimal('price', 8, 2);
             $table->timestamps();
+
         });
     }
 
