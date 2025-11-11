e88f6c02b52a3591155341c027bd4061c835fc9f
Niall Monahan <N00242247@iadt.ie>
2025-11-11 14:57:00 +0000

    Add ticket routes to web.php for ticket management

--- routes/web.php
+++ routes/web.php
@@ -3,6 +3,8 @@
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\ProfileController;
 use App\Http\Controllers\EventController;
+use App\Http\Controllers\TicketController;
+
 
 
 Route::get('/', function () {
@@ -30,6 +32,16 @@ Route::middleware('auth')->group(function () {
 
     //admin
     Route::get('/admin/events', [EventController::class, 'adminIndex'])->name('events.admin');
+
+    // Ticket routes
+    Route::get('/tickets', [TicketController::class, 'index'])->name('tickets.index');
+    Route::get('/tickets/create', [TicketController::class, 'create'])->name('tickets.create');
+    Route::post('/tickets', [TicketController::class, 'store'])->name('tickets.store');
+    Route::get('/tickets/{ticket}', [TicketController::class, 'show'])->name('tickets.show');
+    Route::get('/tickets/{ticket}/edit', [TicketController::class, 'edit'])->name('tickets.edit');
+    Route::put('/tickets/{ticket}', [TicketController::class, 'update'])->name('tickets.update');
+    Route::delete('/tickets/{ticket}', [TicketController::class, 'destroy'])->name('tickets.destroy');
+
 });
 
 
