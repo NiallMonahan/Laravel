d9cbed84741145e9d768deb5f1f35e52634d78ac
niall <N00242247@iadt.ie>
2025-11-27 18:38:38 +0000

    Added the aritist card to the view all

--- resources/views/artists/index.blade.php
+++ resources/views/artists/index.blade.php
@@ -1,20 +1,25 @@
 <x-app-layout>
-    <x-slot name="header">
-        <h2 class="font-semibold text-xl text-gray-800 leading-tight">
-            Artists
-        </h2>
-    </x-slot>
 
-    <div class="py-8 max-w-7xl mx-auto sm:px-6 lg:px-8">
-        @if($artists->count())
+    <!--Title  -->
+    <div class="max-w-[1600px] mx-auto px-4 text-center py-8">
+        <h1
+            class="text-4xl font-extrabold bg-gradient-to-r from-pink-500 to-indigo-400 bg-clip-text text-transparent tracking-tight">
+            All Artists
+        </h1>
+    </div>
+
+    <!-- Artists List -->
+    @if($artists->isEmpty())
+        <p class="text-gray-600">No artists found.</p>
+    @else
+        <div class="py-8 max-w-7xl mx-auto sm:px-6 lg:px-8">
             <ul>
                 @foreach($artists as $artist)
-                    <li>{{ $artist->name }}</li>
+                    <div class="space-y-3">
+                        <x-artist-card :artist="$artist" />
+                    </div>
                 @endforeach
             </ul>
-        @else
-            <p>No artists found.</p>
-        @endif
-
-    </div>
+        </div>
+    @endif
 </x-app-layout>
